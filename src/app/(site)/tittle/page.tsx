8a09d822b6c40fd8b349aa644429d31a796ef866
Hepik <hepiky@gmail.com>
2024-09-02 14:18:59 +0200

    fix: typo in tittle-page

--- src/app/(site)/tittle/page.tsx
+++ src/app/(site)/tittle/page.tsx
@@ -3,7 +3,7 @@ import { Container } from "@/components/shared/Container/Container";
 import TittleSection from "./_components/TittleSection/TittleSection";
 import ReviewsSection from "./_components/ReviewsSection/ReviewsSection";
 
-const HomePage = () => {
+const TittlePage = () => {
   return (
     <div className="flex flex-col space-y-4">
       <Container className="flex flex-col gap-4 flex-1 justify-center py-4 bg-gray-900 rounded-lg border border-black md:justify-start">
@@ -16,4 +16,4 @@ const HomePage = () => {
   );
 };
 
-export default HomePage;
+export default TittlePage;
