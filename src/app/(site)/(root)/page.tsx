5cb2591c4bc82b30f10ec84266879cdb608f9d52
Hepik <hepiky@gmail.com>
2025-01-11 20:11:06 +0100

    automation: debug api endpoint url

--- src/app/(site)/(root)/page.tsx
+++ src/app/(site)/(root)/page.tsx
@@ -4,6 +4,7 @@ import { CarouselHomePage } from "./_components/Carousel/carouselHomePage";
 import { TitleList } from "./_components/TitleList/titleList";
 
 const HomePage = () => {
+  console.log(process.env.NEXT_PUBLIC_API_URL);
   return (
     <Container className="flex flex-col gap-4 flex-1 justify-center py-4 bg-gray-900 rounded-lg border border-black md:justify-start">
       <CarouselHomePage />
